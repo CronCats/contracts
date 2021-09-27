ebf156b1dadb0e7d43a853a882e5a837b968a3ea
trevorjtclarke <trevorjtclarke@gmail.com>
2021-09-26 20:40:09 -0700

    added croncat scheduling proposal demo

--- dao/dao_proposals.sh
+++ dao/dao_proposals.sh
@@ -13,3 +13,6 @@ export NODE_ENV=mainnet
 # near call $DAO_ACCOUNT act_proposal '{"id": 0, "action" :"VoteApprove"}' --accountId $MASTER_ACC  --gas 300000000000000
 # near call $DAO_ACCOUNT act_proposal '{"id": 0, "action" :"VoteReject"}' --accountId $MASTER_ACC  --gas 300000000000000
 # near call $DAO_ACCOUNT act_proposal '{"id": 0, "action" :"VoteRemove"}' --accountId $MASTER_ACC  --gas 300000000000000
+
+# CRONCAT Scheduling proposal example
+# near call $DAO_ACCOUNT add_proposal '{"proposal": {"description": "demo croncat test", "kind": {"FunctionCall": {"receiver_id": "crud.in.testnet", "actions": [{"method_name": "tick", "args": "e30=", "deposit": "0", "gas": "20000000000000"}]}}}}' --accountId $MASTER_ACC --amount 1
