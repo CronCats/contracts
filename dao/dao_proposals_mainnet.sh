81cda98def03787a0ae67d5e2dd91903ec8558c8
trevorjtclarke <trevorjtclarke@gmail.com>
2021-10-05 14:33:41 -0700

    setup staking proposals

--- dao/dao_proposals_mainnet.sh
+++ dao/dao_proposals_mainnet.sh
@@ -8,7 +8,7 @@ export NEAR_ENV=mainnet
 
 ## NOTE: Examples setup as needed, adjust variables for use cases.
 # near view $DAO_ACCOUNT get_policy
-# near call $DAO_ACCOUNT act_proposal '{"id": 1, "action" :"VoteApprove"}' --accountId $MASTER_ACC  --gas 300000000000000
+# near call $DAO_ACCOUNT act_proposal '{"id": 0, "action" :"VoteApprove"}' --accountId $MASTER_ACC  --gas 300000000000000
 # near call $DAO_ACCOUNT act_proposal '{"id": 0, "action" :"VoteReject"}' --accountId $MASTER_ACC  --gas 300000000000000
 # near call $DAO_ACCOUNT act_proposal '{"id": 0, "action" :"VoteRemove"}' --accountId $MASTER_ACC  --gas 300000000000000
 
@@ -25,3 +25,18 @@ export NEAR_ENV=mainnet
 
 ## CRONCAT Scheduling proposal example
 # near call $DAO_ACCOUNT add_proposal '{"proposal": {"description": "demo croncat test", "kind": {"FunctionCall": {"receiver_id": "crud.in.testnet", "actions": [{"method_name": "tick", "args": "e30=", "deposit": "0", "gas": "20000000000000"}]}}}}' --accountId $MASTER_ACC --amount 0.1
+
+## METAPOOL STAKING
+METAPOOL_ACCT=meta-pool.near
+# Stake
+# 10 NEAR
+# STAKE_AMOUNT_NEAR=10000000000000000000000000
+# near call $DAO_ACCOUNT add_proposal '{"proposal": {"description": "Stake funds from croncat dao to metapool", "kind": {"FunctionCall": {"receiver_id": "'$METAPOOL_ACCT'", "actions": [{"method_name": "deposit_and_stake", "args": "e30=", "deposit": "'$STAKE_AMOUNT_NEAR'", "gas": "20000000000000"}]}}}}' --accountId $MASTER_ACC --amount 0.1
+
+# # Unstake all (example: 9xVyewMkzxHfRGtx3EyG82mXX8CfPXLJeW4Xo2y6PpXX)
+# ARGS=`echo "{ \"amount\": \"10000000000000000000000000\" }" | base64`
+# FIXED_ARGS=`echo $ARGS | tr -d '\r' | tr -d ' '`
+# near call $DAO_ACCOUNT add_proposal '{"proposal": {"description": "Unstake all funds from metapool to croncat dao", "kind": {"FunctionCall": {"receiver_id": "'$METAPOOL_ACCT'", "actions": [{"method_name": "unstake", "args": "'$FIXED_ARGS'", "deposit": "0", "gas": "20000000000000"}]}}}}' --accountId $MASTER_ACC --amount 0.1
+
+# # Withdraw balance back (example: EKZqArNzsjq9hpYuYt37Y59qU1kmZoxguLwRH2RnDELd)
+# near call $DAO_ACCOUNT add_proposal '{"proposal": {"description": "Withdraw unstaked funds from metapool to croncat dao", "kind": {"FunctionCall": {"receiver_id": "'$METAPOOL_ACCT'", "actions": [{"method_name": "withdraw_unstaked", "args": "e30=", "deposit": "0", "gas": "20000000000000"}]}}}}' --accountId $MASTER_ACC --amount 0.1
--- dao/dao_proposals_testnet.sh
+++ dao/dao_proposals_testnet.sh
@@ -1,14 +1,14 @@
 set -e
-MASTER_ACC=in.testnet
+MASTER_ACC=cron.testnet
 DAO_ROOT_ACC=sputnikv2.testnet
-DAO_NAME=croncat_testnet_v2
+DAO_NAME=croncat
 DAO_ACCOUNT=$DAO_NAME.$DAO_ROOT_ACC
 
 export NEAR_ENV=testnet
 
 ## NOTE: Examples setup as needed, adjust variables for use cases.
 # near view $DAO_ACCOUNT get_policy
-# near call $DAO_ACCOUNT act_proposal '{"id": 1, "action" :"VoteApprove"}' --accountId $MASTER_ACC  --gas 300000000000000
+near call $DAO_ACCOUNT act_proposal '{"id": 5, "action" :"VoteApprove"}' --accountId $MASTER_ACC  --gas 300000000000000
 # near call $DAO_ACCOUNT act_proposal '{"id": 0, "action" :"VoteReject"}' --accountId $MASTER_ACC  --gas 300000000000000
 # near call $DAO_ACCOUNT act_proposal '{"id": 0, "action" :"VoteRemove"}' --accountId $MASTER_ACC  --gas 300000000000000
 
@@ -25,3 +25,18 @@ export NEAR_ENV=testnet
 
 ## CRONCAT Scheduling proposal example
 # near call $DAO_ACCOUNT add_proposal '{"proposal": {"description": "demo croncat test", "kind": {"FunctionCall": {"receiver_id": "crud.in.testnet", "actions": [{"method_name": "tick", "args": "e30=", "deposit": "0", "gas": "20000000000000"}]}}}}' --accountId $MASTER_ACC --amount 0.1
+
+
+
+# ## METAPOOL STAKING
+# METAPOOL_ACCT=meta-v2.pool.testnet
+# # Stake (example: 5jh8NP6dwXUELQfTSZSQzKpyaPjuue8btEaxv1Ng1MBT, and https://explorer.testnet.near.org/transactions/4rgrYB9W1UxZVzyVeYRP6sAGsWv18tfsB3zX3KjyYqqF)
+# near call $DAO_ACCOUNT add_proposal '{"proposal": {"description": "Stake some funds from croncat dao to metapool", "kind": {"FunctionCall": {"receiver_id": "'$METAPOOL_ACCT'", "actions": [{"method_name": "deposit_and_stake", "args": "e30=", "deposit": "10000000000000000000000000", "gas": "20000000000000"}]}}}}' --accountId $MASTER_ACC --amount 0.1
+
+# # Unstake all (example: 9xVyewMkzxHfRGtx3EyG82mXX8CfPXLJeW4Xo2y6PpXX)
+# ARGS=`echo "{ \"amount\": \"10000000000000000000000000\" }" | base64`
+# FIXED_ARGS=`echo $ARGS | tr -d '\r' | tr -d ' '`
+# near call $DAO_ACCOUNT add_proposal '{"proposal": {"description": "Unstake all funds from metapool to croncat dao", "kind": {"FunctionCall": {"receiver_id": "'$METAPOOL_ACCT'", "actions": [{"method_name": "unstake", "args": "'$FIXED_ARGS'", "deposit": "0", "gas": "20000000000000"}]}}}}' --accountId $MASTER_ACC --amount 0.1
+
+# # Withdraw balance back (example: EKZqArNzsjq9hpYuYt37Y59qU1kmZoxguLwRH2RnDELd)
+# near call $DAO_ACCOUNT add_proposal '{"proposal": {"description": "Withdraw unstaked funds from metapool to croncat dao", "kind": {"FunctionCall": {"receiver_id": "'$METAPOOL_ACCT'", "actions": [{"method_name": "withdraw_unstaked", "args": "e30=", "deposit": "0", "gas": "20000000000000"}]}}}}' --accountId $MASTER_ACC --amount 0.1
