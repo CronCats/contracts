e9e8bf1018a245a58e1b4ff6592b9416cd06d5d6
trevorjtclarke <trevorjtclarke@gmail.com>
2021-11-18 11:34:40 -0800

    PoC worked, need to refactor and check validity of solution

--- manager/src/triggers.rs
+++ manager/src/triggers.rs
@@ -86,260 +86,161 @@ impl Contract {
         Promise::new(trigger.owner_id).transfer(self.trigger_storage_usage as u128);
     }
 
-    // /// Method for agent to view & evaluate range of view calls
-    // /// Callable by anyone, but only active agents can execute
-    // // Based on active agent index, agent is responsible to a range of view calls, 
-    // // if any evaluate to true return the range index, as long as the pagination sort doesnt change, this could work. 
-    // // Does knowing the RANGE give enough information for agent to skew execution?
-    // // Bonus here is the view ranges can scale with the view needs, as agents could get assigned multiple ranges potentially that are async RPC calls
-    // pub fn proxy_view(&self) -> Base64VecU8 {
-    //   let view_results: Vec<bool> = Vec::new();
-    //   // TODO:
-    //   // get a range of view triggers
-    //   // Loop and call each
-    //   // Create a that represents if the range has any TRUE results
-    //   let start = 0; // TODO: Change to better range management
-    //   let end = self.triggers.len();
+    /// Returns trigger data
+    ///
+    /// ```bash
+    /// near view cron.testnet get_triggers '{"from_index": 0, "limit": 10}'
+    /// ```
+    pub fn get_triggers(
+        &self,
+        from_index: Option<U64>,
+        limit: Option<U64>,
+    ) -> Vec<Trigger> {
+        let mut ret: Vec<Trigger> = Vec::new();
+        let mut start = 0;
+        let mut end = 10;
+        if let Some(from_index) = from_index {
+            start = from_index.0;
+        }
+        if let Some(limit) = limit {
+            end = u64::min(start + limit.0, self.tasks.len());
+        }
 
-    //   // Return all tasks within range
-    //   let keys = self.triggers.keys_as_vector();
-    //   for i in start..end {
-    //     if let Some(trigger_hash) = keys.get(i) {
-    //       if let Some(trigger) = self.triggers.get(&trigger_hash) {
-    //         if let Some(task) = self.tasks.get(&trigger.task_hash.0.as_slice()) {
-    //           let promise_first = env::promise_create(
-    //             trigger.contract_id.clone(),
-    //             &trigger.function_id.as_bytes(),
-    //             trigger.arguments.0.as_slice(),
-    //             NO_DEPOSIT,
-    //             VIEW_CALL_GAS,
-    //           );
-    //           let promise_second = env::promise_then(
-    //             promise_first,
-    //             task.contract_id.clone(),
-    //             &task.function_id.as_bytes(),
-    //             task.arguments.0.as_slice(),
-    //             task.deposit.0,
-    //             task.gas,
-    //           );
-    //           env::promise_return(promise_second);
-    //           env::promise_batch_create(account_id)
-    //         }
-    //       }
-    //     }
-    //   }
+        // Return all tasks within range
+        let keys = self.triggers.keys_as_vector();
+        for i in start..end {
+            if let Some(trigger_hash) = keys.get(i) {
+                if let Some(trigger) = self.triggers.get(&trigger_hash) {
+                    // TODO: Add the computed trigger hash here!
+                    ret.push(trigger);
+                }
+            }
+        }
+        ret
+    }
 
-    //   let input = format!(
-    //       "{:?}{:?}{:?}{:?}",
-    //       item.contract_id, item.function_id, item.task_hash, item.owner_id
-    //   );
-    //   let hash = env::sha256(input.as_bytes());
-    //   Base64VecU8::from(hash)
-    // }
+    /// Allows agents to check if a view method should trigger a task immediately
+    ///
+    /// TODO:
+    /// - Check for range hash 
+    /// - Loop range to find view BOOL TRUE
+    /// - Get task details
+    /// - Execute task
+    ///
+    /// ```bash
+    /// near call cron.testnet proxy_conditional_call '{"trigger_hash": ""}' --accountId YOU.testnet
+    /// ```
+    pub fn proxy_conditional_call(&mut self, trigger_hash: Base64VecU8) {
+        // No adding tasks while contract is paused
+        assert_eq!(self.paused, false, "Task execution paused");
 
-    /// Method for agent to view & evaluate range of view calls
-    /// DEMO ONLY
-    pub fn proxy_view(&self) {
-      // let view_results: Vec<bool> = Vec::new();
-      // // TODO:
-      // // get a range of view triggers
-      // // Loop and call each
-      // // Create a that represents if the range has any TRUE results
-      // let start = 0; // TODO: Change to better range management
-      // let end = self.triggers.len();
+        // only registered agent signed, because micropayments will benefit long term
+        let agent_opt = self.agents.get(&env::predecessor_account_id());
+        if agent_opt.is_none() {
+            env::panic(b"Agent not registered");
+        }
 
-      // Return all tasks within range
-      let keys = self.triggers.keys_as_vector();
-      if let Some(trigger_hash) = keys.get(0) {
-        if let Some(trigger) = self.triggers.get(&trigger_hash) {
-          let promise_first = env::promise_create(
+        let trigger = self.triggers.get(&trigger_hash.into()).expect("No task found by hash");
+
+        // Call external contract with task variables
+        let promise_first = env::promise_create(
             trigger.contract_id.clone(),
             &trigger.function_id.as_bytes(),
             trigger.arguments.0.as_slice(),
             NO_DEPOSIT,
             VIEW_CALL_GAS,
-          );
-          let promise_second = env::promise_then(
+        );
+        let promise_second = env::promise_then(
             promise_first,
             env::current_account_id(),
-            b"callback_for_proxy_view",
+            b"proxy_conditional_callback",
             json!({
-              "task_hash": trigger.task_hash
+                "task_hash": trigger.task_hash,
+                "agent_id": &env::predecessor_account_id(),
             })
             .to_string()
             .as_bytes(),
-            0,
+            NO_DEPOSIT,
             GAS_FOR_CALLBACK,
-          );
-          env::promise_return(promise_second);
-        }
-      }
+        );
+        env::promise_return(promise_second);
     }
 
-    /// NOTE: Testing
+    /// Callback, if response is TRUE, then do the actual proxy call
     #[private]
-    pub fn callback_for_proxy_view(&mut self, task_hash: Base64VecU8) -> bool {
-      assert_eq!(
-        env::promise_results_count(),
-        1,
-        "Expected 1 promise result."
-      );
-      // TODO: Could loop the promise results here
-      match env::promise_result(0) {
-        PromiseResult::NotReady => {
-          unreachable!()
-        }
-        PromiseResult::Successful(trigger_result) => {
-          log!("trigger_result {:?}", &trigger_result);
-          let result: bool = serde_json::de::from_slice(&trigger_result)
-            .expect("Could not get result from trigger");
-          log!("results {:?} {:?}", result, &task_hash);
+    pub fn proxy_conditional_callback(&mut self, task_hash: Base64VecU8, agent_id: AccountId) {
+        assert_eq!(
+            env::promise_results_count(),
+            1,
+            "Expected 1 promise result."
+        );
+        match env::promise_result(0) {
+            PromiseResult::NotReady => {
+                unreachable!()
+            }
+            PromiseResult::Successful(trigger_result) => {
+                let result: bool = serde_json::de::from_slice(&trigger_result)
+                    .expect("Could not get result from trigger");
 
-          result
+                // TODO: Refactor to re-used method
+                if result {
+                    let mut agent = self.agents.get(&agent_id).expect("Agent not found");
+                    let mut task = self.tasks.get(&task_hash.clone().into()).expect("No task found by hash");
+
+                    // Fee breakdown:
+                    // - Used Gas: Task Txn Fee Cost
+                    // - Agent Fee: Incentivize Execution SLA
+                    //
+                    // Task Fee Examples:
+                    // Total Fee = Gas Fee + Agent Fee
+                    // Total Balance = Task Deposit + Total Fee
+                    //
+                    // NOTE: Gas cost includes the cross-contract call & internal logic of this contract.
+                    // Direct contract gas fee will be lower than task execution costs, however
+                    // we require the task owner to appropriately estimate gas for overpayment.
+                    // The gas overpayment will also accrue to the agent since there is no way to read
+                    // how much gas was actually used on callback.
+                    let call_fee_used = u128::from(task.gas) * self.gas_price;
+                    let call_total_fee = call_fee_used + self.agent_fee;
+                    let call_total_balance = task.deposit.0 + call_total_fee;
+
+                    // Update agent storage
+                    // Increment agent reward & task count
+                    // Reward for agent MUST include the amount of gas used as a reimbursement
+                    agent.balance = U128::from(agent.balance.0 + call_total_fee);
+                    agent.total_tasks_executed = U128::from(agent.total_tasks_executed.0 + 1);
+                    self.available_balance = self.available_balance - call_total_fee;
+
+                    // Reset missed slot, if any
+                    if agent.last_missed_slot != 0 {
+                        agent.last_missed_slot = 0;
+                    }
+                    self.agents.insert(&env::signer_account_id(), &agent);
+
+                    // Decrease task balance, Update task storage
+                    task.total_deposit = U128::from(task.total_deposit.0 - call_total_balance);
+                    self.tasks.insert(&task_hash.into(), &task);
+
+                    // Call external contract with task variables
+                    let promise_first = env::promise_create(
+                        task.contract_id.clone(),
+                        &task.function_id.as_bytes(),
+                        task.arguments.0.as_slice(),
+                        task.deposit.0,
+                        task.gas,
+                    );
+
+                    env::promise_return(promise_first);
+                } else {
+                    log!("Trigger returned false");
+                }
+            }
+            PromiseResult::Failed => {
+                // Problem with the creation transaction, reward money has been returned to this contract.
+                log!("Trigger call failed");
+            }
         }
-        PromiseResult::Failed => {
-          // Problem with the creation transaction, reward money has been returned to this contract.
-          log!("No results");
-          false
-        }
-      }
     }
-
-    // /// Allows agents to check if a view method should trigger a task immediately
-    // ///
-    // /// TODO:
-    // /// - Check for range hash 
-    // /// - Loop range to find view BOOL TRUE
-    // /// - Get task details
-    // /// - Execute task
-    // ///
-    // /// ```bash
-    // /// near call cron.testnet proxy_call --accountId YOU.testnet
-    // /// ```
-    // pub fn proxy_call_conditional(&mut self, range_hash: Base64VecU8) {
-    //     // No adding tasks while contract is paused
-    //     assert_eq!(self.paused, false, "Task execution paused");
-
-    //     // only registered agent signed, because micropayments will benefit long term
-    //     let agent_opt = self.agents.get(&env::predecessor_account_id());
-    //     if agent_opt.is_none() {
-    //         env::panic(b"Agent not registered");
-    //     }
-    //     let mut agent = agent_opt.unwrap();
-
-    //     // Get current slot based on block or timestamp
-    //     let current_slot = self.get_slot_id(None);
-
-    //     // get task based on current slot
-    //     // priority goes to tasks that have fallen behind (using floor key)
-    //     let (slot_opt, slot_ballpark) = if let Some(k) = self.slots.floor_key(&current_slot) {
-    //         (self.slots.get(&k), k)
-    //     } else {
-    //         (self.slots.get(&current_slot), current_slot)
-    //     };
-
-    //     let mut slot_data = slot_opt.expect("No tasks found in slot");
-
-    //     // Check if agent has exceeded their slot task allotment
-    //     // TODO: An agent can check to execute IF slot is +1 and their index is within range???
-    //     let (can_execute, current_agent_index, _) =
-    //         self.check_agent_can_execute(env::predecessor_account_id(), slot_data.len() as u64);
-    //     assert!(can_execute, "Agent has exceeded execution for this slot");
-    //     // Rotate agent index
-    //     if self.agent_active_index as u64 == self.agent_active_queue.len().saturating_sub(1) {
-    //         self.agent_active_index = 0;
-    //     } else if self.agent_active_queue.len() > 1 {
-    //         // Only change the index IF there are more than 1 agents ;)
-    //         self.agent_active_index += 1;
-    //     }
-    //     // IF previous agent missed, then store their slot missed. We know this is true IF this slot is using slot_ballpark
-    //     // NOTE: While this isnt perfect, the eventual outcome is fine.
-    //     //       If agent gets ticked as "missed" for maximum of 1 slot, then fixes the situation on next round.
-    //     //       If agent truly misses enough slots, they will skip their chance to reset missed slot count and be dropped.
-    //     if slot_ballpark < current_slot && self.agent_active_queue.len() > 1 {
-    //         // wrap around logic for non-overflow index
-    //         // if only 1 agent, dont do anything
-    //         let missed_agent_index = if current_agent_index == 0 {
-    //             self.agent_active_queue.len()
-    //         } else {
-    //             current_agent_index - 1
-    //         };
-    //         let missed_agent_id = self.agent_active_queue.get(missed_agent_index);
-
-    //         if let Some(missed_agent_id) = missed_agent_id {
-    //             let missed_agent = self.agents.get(&missed_agent_id);
-
-    //             // confirm we should update missed slot, ONLY if the slot id is 0, otherwise the agent has not reset the count and we shouldnt mess with it.
-    //             if let Some(missed_agent) = missed_agent {
-    //                 let mut m_agent = missed_agent;
-    //                 if m_agent.last_missed_slot == 0 {
-    //                     m_agent.last_missed_slot = slot_ballpark;
-    //                     // update storage
-    //                     self.agents.insert(&missed_agent_id, &m_agent);
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     // Get a single task hash, then retrieve task details
-    //     let hash = slot_data.pop().expect("No tasks available");
-
-    //     // After popping, ensure state is rewritten back
-    //     if slot_data.is_empty() {
-    //         // Clean up slot if no more data
-    //         self.slots.remove(&slot_ballpark);
-    //         // log!("Slot {} cleaned", &slot_ballpark);
-    //     } else {
-    //         self.slots.insert(&slot_ballpark, &slot_data);
-    //     }
-
-    //     let mut task = self.tasks.get(&hash).expect("No task found by hash");
-
-    //     // Fee breakdown:
-    //     // - Used Gas: Task Txn Fee Cost
-    //     // - Agent Fee: Incentivize Execution SLA
-    //     //
-    //     // Task Fee Examples:
-    //     // Total Fee = Gas Fee + Agent Fee
-    //     // Total Balance = Task Deposit + Total Fee
-    //     //
-    //     // NOTE: Gas cost includes the cross-contract call & internal logic of this contract.
-    //     // Direct contract gas fee will be lower than task execution costs, however
-    //     // we require the task owner to appropriately estimate gas for overpayment.
-    //     // The gas overpayment will also accrue to the agent since there is no way to read
-    //     // how much gas was actually used on callback.
-    //     let call_fee_used = u128::from(task.gas) * self.gas_price;
-    //     let call_total_fee = call_fee_used + self.agent_fee;
-    //     let call_total_balance = task.deposit.0 + call_total_fee;
-
-    //     // Update agent storage
-    //     // Increment agent reward & task count
-    //     // Reward for agent MUST include the amount of gas used as a reimbursement
-    //     agent.balance = U128::from(agent.balance.0 + call_total_fee);
-    //     agent.total_tasks_executed = U128::from(agent.total_tasks_executed.0 + 1);
-    //     self.available_balance = self.available_balance - call_total_fee;
-
-    //     // Reset missed slot, if any
-    //     if agent.last_missed_slot != 0 {
-    //         agent.last_missed_slot = 0;
-    //     }
-    //     self.agents.insert(&env::signer_account_id(), &agent);
-
-    //     // Decrease task balance, Update task storage
-    //     task.total_deposit = U128::from(task.total_deposit.0 - call_total_balance);
-    //     self.tasks.insert(&hash, &task);
-
-    //     // Call external contract with task variables
-    //     let promise_first = env::promise_create(
-    //         task.contract_id.clone(),
-    //         &task.function_id.as_bytes(),
-    //         task.arguments.0.as_slice(),
-    //         task.deposit.0,
-    //         task.gas,
-    //     );
-
-    //     env::promise_return(promise_first);
-    // }
 }
 
 // Internal methods
